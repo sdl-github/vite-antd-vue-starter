72c4a495368f0df3d7d0df39c58f5c781019ce35
sdl-code <sdl-life@foxmai.com>
2022-07-12 10:42:34 +0800

    菜单弹出

--- src/views/system/role/components/PermissionSelect.vue
+++ src/views/system/role/components/PermissionSelect.vue
@@ -2,6 +2,7 @@
 import { queryMenuTree } from '@/api/menu';
 import { onMounted, ref, watch, watchEffect } from 'vue';
 import { message, TreeSelect } from 'ant-design-vue';
+import RemixIcon from '@/components/RemixIcon.vue';
 const fileName = {
     children: 'children',
     title: 'name',
@@ -11,6 +12,10 @@ const props = defineProps({
     currentRole: {
         type: Object,
         default: () => { },
+    },
+    visible: {
+        type: Boolean,
+        default: false,
     },
 });
 const treeData = ref([]);
@@ -39,8 +44,9 @@ async function initData() {
 }
 
 function handleCheck(e) {
-    const { halfChecked, checked } = e
-    checkPermissionKeys.value = [...halfChecked, ...checked]
+    console.log(e);
+    // const { halfChecked, checked } = e
+    // checkPermissionKeys.value = [...halfChecked, ...checked]
 }
 
 function handleSave() {
@@ -56,20 +62,18 @@ function handleSave() {
 </script>
 
 <template>
-    <a-card class="permission_card" title="权限配置">
+    <a-drawer v-model:visible="visible" title="权限配置" placement="right" @close="emits('handleCancel')">
         <template #extra>
-            <a-button class="mr-2" v-if="currentRole.name" @click="emits('handleCancel')">取消</a-button>
+            <a-button class="mr-2" @click="emits('handleCancel')">取消</a-button>
             <a-button @click="handleSave" type="primary">保存</a-button>
         </template>
         <div class="mb-2 ml-2 px-2 py-1 bg-purple-100 dark:bg-purple-100 text-xs font-semibold text-purple-800 dark:text-purple-800 rounded uppercase"
             v-if="currentRole.name"> 当前选择： {{ currentRole.name }}</div>
-        <a-tree @check="handleCheck" v-model:checkedKeys="checkedKeys" :selectable="false" :tree-data="treeData"
-            :field-names="fileName" checkable checkStrictly>
-            <template #icon="{ key, selected }">
-                fuck
-            </template>
+        <a-tree @check="handleCheck" default-expand-all v-model:checkedKeys="checkedKeys" :selectable="false"
+            :tree-data="treeData" :field-names="fileName" checkable>
         </a-tree>
-    </a-card>
+    </a-drawer>
+
 </template>
 
 <style lang="scss" scoped>
