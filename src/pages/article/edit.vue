3be4bd95889dc748ad37e877309d43897c5ef892
sdl-code <sdl-life@foxmail.com>
2024-03-04 09:07:31 +0800

    fix type

--- src/pages/article/edit.vue
+++ src/pages/article/edit.vue
@@ -4,20 +4,21 @@
 </route>
 
 <script setup lang="ts">
-import { message } from 'ant-design-vue';
-import { FormModel, generateFormModel } from './data';
-import { createArticle, queryArticle, updateArticle } from '~/api/article';
-import { useModal } from '~/components/ArticlePublishModal.vue';
+import { message } from 'ant-design-vue'
+import type { FormModel } from './data'
+import { generateFormModel } from './data'
+import { createArticle, queryArticle, updateArticle } from '~/api/article'
+import { useModal } from '~/components/ArticlePublishModal.vue'
 
 const route = useRoute()
 const loading = ref(true)
 const saveLoading = ref(false)
 const router = useRouter()
 const state = reactive<{ form: FormModel }>({
-  form: generateFormModel()
+  form: generateFormModel(),
 })
 
-const { form} = toRefs(state)
+const { form } = toRefs(state)
 
 const articlePublishModal = useModal()
 
@@ -30,16 +31,21 @@ watchEffect(() => {
   loading.value = false
 })
 
-
 async function initData(id: string) {
   loading.value = true
   const { queryArticle: res } = await queryArticle(id)
   const { title, html, markdown, metaDescription, metaTitle, image, categoryId } = res as FormModel
   state.form = {
-    id, title, html, markdown, metaDescription, metaTitle, image, categoryId
+    id,
+    title,
+    html,
+    markdown,
+    metaDescription,
+    metaTitle,
+    image,
+    categoryId,
   }
   loading.value = false
-  console.log(form);
 }
 
 async function handleSave() {
@@ -47,19 +53,16 @@ async function handleSave() {
   const loading = message.loading('加载中', 0)
   try {
     const data = unref(form)
-    console.log(data);
-    
+
     const api = data.id ? updateArticle : createArticle
-    console.log(api);
-    
-    if (!data.id) {
+
+    if (!data.id)
       delete data.id
-    }
+
     const res = await api(data)
-    console.log(res);
-    if (res.createArticle) {
-      form.id = res.createArticle.id
-    }
+    if ('createArticle' in res)
+      state.form.id = res.createArticle?.id
+
     loading()
     saveLoading.value = false
     message.success('成功')
@@ -79,21 +82,29 @@ async function handlePublish() {
 
 <template>
   <div class="w-full p-2">
-
     <ArticlePublishModal />
 
     <div class="rounded bg-white">
-      <APageHeader class="px-4 py-2" title="返回" sub-title="新建/编辑文章" @back="() => router.push('/article')">
+      <APageHeader class="px-4 py-2" sub-title="新建/编辑文章" @back="() => router.push('/article')">
+        <template #title>
+          <div class="cursor-pointer">
+            返回
+          </div>
+        </template>
         <template #extra>
-          <a-button :loading="saveLoading" @click="handleSave">保存</a-button>
-          <a-button @click="handlePublish" type="primary">发布</a-button>
+          <AButton :loading="saveLoading" @click="handleSave">
+            保存
+          </AButton>
+          <AButton type="primary" @click="handlePublish">
+            发布
+          </AButton>
         </template>
       </APageHeader>
     </div>
 
-    <div class="mt-2 rounded bg-white p-2 min-h-40vh">
+    <div class="mt-2 min-h-40vh rounded bg-white p-2">
       <div v-if="loading" class="h-100vh flex justify-center pt-10">
-        <div class="flex items-center flex-col">
+        <div class="flex flex-col items-center">
           <ASpin />
           <div>加载中</div>
         </div>
